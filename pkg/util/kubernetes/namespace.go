8ffbabaf6d72e512bddb690cb7d085bebfacdcdd
Charles Moulliard <cmoulliard@redhat.com>
2018-10-23 13:19:52 +0200

    add new functions

--- pkg/util/kubernetes/namespace.go
+++ pkg/util/kubernetes/namespace.go
@@ -6,16 +6,25 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+func SetNamespace(resource interface{}, component *v1alpha1.Component) {
+	if obj, ok := resource.(metav1.Object); ok {
+		obj.SetNamespace(component.ObjectMeta.GetNamespace())
+	}
+}
+
+func SetOwnerReferences(obj metav1.Object, component *v1alpha1.Component) {
+	obj.SetOwnerReferences([]metav1.OwnerReference{
+		*metav1.NewControllerRef(component, schema.GroupVersionKind{
+			Group:   v1alpha1.SchemeGroupVersion.Group,
+			Version: v1alpha1.SchemeGroupVersion.Version,
+			Kind:    component.Kind,
+		}),
+	})
+}
+
 func SetNamespaceAndOwnerReference(resource interface{}, component *v1alpha1.Component) {
 	if obj, ok := resource.(metav1.Object); ok {
 		obj.SetNamespace(component.ObjectMeta.GetNamespace())
-
-		obj.SetOwnerReferences([]metav1.OwnerReference{
-			*metav1.NewControllerRef(component, schema.GroupVersionKind{
-				Group:   v1alpha1.SchemeGroupVersion.Group,
-				Version: v1alpha1.SchemeGroupVersion.Version,
-				Kind:    component.Kind,
-			}),
-		})
+		SetOwnerReferences(obj, component)
 	}
 }
