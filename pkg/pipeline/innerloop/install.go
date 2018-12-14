c3a4413236df58975c7e1c5c6497dde54e7621bc
Charles Moulliard <cmoulliard@redhat.com>
2018-12-14 19:30:53 +0100

    Add new templates supporting Deployment, Ingress and change logic to detect if the platform is k8s or openshift

--- pkg/pipeline/innerloop/install.go
+++ pkg/pipeline/innerloop/install.go
@@ -55,93 +55,127 @@ func (installStep) Handle(component *v1alpha1.Component, client *client.Client, 
 func installInnerLoop(component *v1alpha1.Component, c client.Client, namespace string) error {
 	component.ObjectMeta.Namespace = namespace
 	// Append dev runtime's image (java, nodejs, ...)
-	component.Spec.RuntimeName = strings.Join([]string{"dev-runtime",strings.ToLower(component.Spec.Runtime)},"-")
+	component.Spec.RuntimeName = strings.Join([]string{"dev-runtime", strings.ToLower(component.Spec.Runtime)}, "-")
 	component.Spec.Storage.Name = "m2-data-" + component.Name
 
-	// TODO Add a key to get the templates associated to a category such as : innerloop, ....
-	for _, tmpl := range util.Templates {
-		if strings.HasPrefix(tmpl.Name(), "innerloop") {
+	isOpenshift, err := kubernetes.DetectOpenShift()
+	if err != nil {
+		return err
+	}
 
-			switch tmpl.Name() {
-			case "innerloop/imagestream":
-				// Get supervisord imagestream
-				component.Spec.Images = GetSupervisordImage()
+	if (isOpenshift) {
+		tmpl, ok := util.Templates["innerloop/imagestream"]
+		if ok {
+			component.Spec.Images = GetSupervisordImage()
 
-				// Define the key of the image to search accoring to the runtime
-				imageKey := ""
-				switch r := component.Spec.Runtime; r {
-				case "spring-boot", "vert.x", "thornthail":
-					imageKey = "java"
-				case "nodejs":
-					imageKey = "nodejs"
-				default:
-					imageKey = "java"
-				}
-
-				component.Spec.Images = append(component.Spec.Images, CreateTypeImage(true, component.Spec.RuntimeName, "latest", image[imageKey], false))
-
-				err := createResource(tmpl, component, c)
-				if err != nil {
-					return err
-				}
-
-				log.Infof("#### Created 'supervisord and '%s' imagestreams", image[imageKey])
-
-			case "innerloop/pvc":
-				component.Spec.Storage.Capacity = "1Gi"
-				component.Spec.Storage.Mode = "ReadWriteOnce"
-				err := createResource(tmpl, component, c)
-				if err != nil {
-					return err
-				}
-				log.Infof("#### Created '%s' persistent volume storage; capacity: '%s'; mode '%s'", component.Spec.Storage.Name, component.Spec.Storage.Capacity, component.Spec.Storage.Mode)
-
-			case "innerloop/deploymentconfig":
-				if component.Spec.Port == 0 {
-					component.Spec.Port = 8080 // Add a default port if empty
-				}
-				component.Spec.SupervisordName = "copy-supervisord"
-
-				// Enrich Env Vars with Default values
-				populateEnvVar(component)
-
-				err := createResource(tmpl, component, c)
-				if err != nil {
-					return err
-				}
-				log.Infof("#### Created dev's deployment config containing as initContainer : supervisord")
-
-			case "innerloop/route":
-				if component.Spec.ExposeService {
-					err := createResource(tmpl, component, c)
-					if err != nil {
-						return err
-					}
-					log.Infof("#### Exposed service's port '%d' as cluster's route", component.Spec.Port)
-				}
-
-			case "innerloop/service":
-				if component.Spec.Port == 0 {
-					component.Spec.Port = 8080 // Add a default port if empty
-				}
-				err := createResource(tmpl, component, c)
-				if err != nil {
-					return err
-				}
-				log.Infof("#### Created service's port '%d'", component.Spec.Port)
-
+			// Define the key of the image to search accoring to the runtime
+			imageKey := ""
+			switch r := component.Spec.Runtime; r {
+			case "spring-boot", "vert.x", "thornthail":
+				imageKey = "java"
+			case "nodejs":
+				imageKey = "nodejs"
 			default:
+				imageKey = "java"
+			}
+
+			component.Spec.Images = append(component.Spec.Images, CreateTypeImage(true, component.Spec.RuntimeName, "latest", image[imageKey], false))
+
+			err := createResource(tmpl, component, c)
+			if err != nil {
+				return err
+			}
+
+			log.Infof("#### Created 'supervisord and '%s' imagestreams", image[imageKey])
+		}
+
+		tmpl, ok = util.Templates["innerloop/deploymentconfig"]
+		if ok {
+			if component.Spec.Port == 0 {
+				component.Spec.Port = 8080 // Add a default port if empty
+			}
+			component.Spec.SupervisordName = "copy-supervisord"
+
+			// Enrich Env Vars with Default values
+			populateEnvVar(component)
+
+			err := createResource(tmpl, component, c)
+			if err != nil {
+				return err
+			}
+			log.Infof("#### Created dev's deployment config containing as initContainer : supervisord")
+		}
+
+		tmpl, ok = util.Templates["innerloop/route"]
+		if ok {
+			if component.Spec.ExposeService {
 				err := createResource(tmpl, component, c)
 				if err != nil {
 					return err
 				}
+				log.Infof("#### Exposed service's port '%d' as cluster's route", component.Spec.Port)
 			}
 		}
+	} else {
+		// This is not an OpenShift cluster but instead a K8s platform
+		tmpl, ok := util.Templates["innerloop/deployment"]
+		if ok {
+			if component.Spec.Port == 0 {
+				component.Spec.Port = 8080 // Add a default port if empty
+			}
+			component.Spec.SupervisordName = "copy-supervisord"
+
+			// Enrich Env Vars with Default values
+			populateEnvVar(component)
+
+			err := createResource(tmpl, component, c)
+			if err != nil {
+				return err
+			}
+			log.Infof("#### Created dev's deployment containing as initContainer : supervisord")
+		}
+
+		tmpl, ok = util.Templates["innerloop/ingress"]
+		if ok {
+			if component.Spec.ExposeService {
+				err := createResource(tmpl, component, c)
+				if err != nil {
+					return err
+				}
+				log.Infof("#### Exposed service's port '%d' as cluster's ingress route", component.Spec.Port)
+			}
+		}
+
 	}
+
+	// Install common resources
+	tmpl, ok := util.Templates["innerloop/pvc"]
+	if ok {
+		component.Spec.Storage.Capacity = "1Gi"
+		component.Spec.Storage.Mode = "ReadWriteOnce"
+		err := createResource(tmpl, component, c)
+		if err != nil {
+			return err
+		}
+		log.Infof("#### Created '%s' persistent volume storage; capacity: '%s'; mode '%s'", component.Spec.Storage.Name, component.Spec.Storage.Capacity, component.Spec.Storage.Mode)
+	}
+
+	tmpl, ok = util.Templates["innerloop/service"]
+	if ok {
+		if component.Spec.Port == 0 {
+			component.Spec.Port = 8080 // Add a default port if empty
+		}
+		err := createResource(tmpl, component, c)
+		if err != nil {
+			return err
+		}
+		log.Infof("#### Created service's port '%d'", component.Spec.Port)
+
+	}
+
 	log.Infof("#### Created %s CRD's component ", component.Name)
 	component.Status.Phase = v1alpha1.PhaseDeploying
-  
-	err := c.Update(context.TODO(), component)
+	err = c.Update(context.TODO(), component)
 	if err != nil && k8serrors.IsConflict(err) {
 		return err
 	}
--- pkg/util/kubernetes/config.go
+++ pkg/util/kubernetes/config.go
@@ -18,104 +18,31 @@ limitations under the License.
 package kubernetes
 
 import (
-	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
-	"io/ioutil"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	restclient "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
-	clientcmdlatest "k8s.io/client-go/tools/clientcmd/api/latest"
-	"os/user"
-	"strings"
+	"k8s.io/client-go/discovery"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
-const (
-	KUBECONFILE = ".kube/config"
-)
-
-type Kube struct {
-	MasterURL string
-	Config    string
-}
-
-// InitKubeClient initialize the k8s client
-func InitKubeClient(kubeconfig string) error {
-	if kubeconfig == "" {
-		kubeconfig = getK8Config("")
-	}
-
-	// use the current context in kubeconfig
-	_, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+func DetectOpenShift() (bool,error) {
+	kubeconfig, err := config.GetConfig()
 	if err != nil {
-		return err
+		return false, err
 	}
-
-	return nil
-}
-
-func GetK8RestConfig() *restclient.Config {
-	return createKubeRestconfig()
-}
-
-// Create Kube Rest's Config Client
-func createKubeRestconfig() *restclient.Config {
-	kube := Kube{
-		Config:    HomeKubePath(),
-		MasterURL: "192.168.99.50:8443",
-	}
-	kubeRestClient, err := clientcmd.BuildConfigFromFlags(kube.MasterURL, kube.Config)
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(kubeconfig)
 	if err != nil {
-		log.Fatalf("Error building kubeconfig: %s", err.Error())
+		return false, err
 	}
-	return kubeRestClient
-}
-
-func getK8Config(kubeconfig string) string {
-	log.Debug("Get K8s config file")
-	if kubeconfig == "" {
-		return HomeKubePath()
-	} else {
-		return kubeconfig
-	}
-	log.Debug("Kubeconfig : ", kubeconfig)
-	return ""
-}
-
-func HomeKubePath() string {
-	usr, err := user.Current()
+	apiList, err := discoveryClient.ServerGroups()
 	if err != nil {
-		log.Debugf("Can't get current user:\n%v", err)
+		return false, err
 	}
-	return strings.Join([]string{usr.HomeDir, KUBECONFILE}, "/")
+	apiGroups := apiList.Groups
+	for i := 0; i < len(apiGroups); i++ {
+		if (apiGroups[i].Name == "route.openshift.io") {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
-// GetClientCurrentNamespace --
-func GetClientCurrentNamespace(kubeconfig string) (string, error) {
-	if kubeconfig == "" {
-		kubeconfig = getK8Config(kubeconfig)
-	}
-	if kubeconfig == "" {
-		return "default", nil
-	}
 
-	data, err := ioutil.ReadFile(kubeconfig)
-	if err != nil {
-		return "", err
-	}
-	config := clientcmdapi.NewConfig()
-	if len(data) == 0 {
-		return "", errors.New("kubernetes config file is empty")
-	}
 
-	decoded, _, err := clientcmdlatest.Codec.Decode(data, &schema.GroupVersionKind{Version: clientcmdlatest.Version, Kind: "Config"}, config)
-	if err != nil {
-		return "", err
-	}
-
-	clientcmdconfig := decoded.(*clientcmdapi.Config)
-
-	cc := clientcmd.NewDefaultClientConfig(*clientcmdconfig, &clientcmd.ConfigOverrides{})
-	ns, _, err := cc.Namespace()
-	return ns, err
-}
