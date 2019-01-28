ca251af76cb222f9039891b9f867af831323e3c3
Charles Moulliard <cmoulliard@redhat.com>
2019-01-28 15:29:27 +0100

    Add new util function to get namespace to watch.

--- pkg/util/kubernetes/namespace.go
+++ pkg/util/kubernetes/namespace.go
@@ -1,10 +1,27 @@
 package kubernetes
 
 import (
+	"fmt"
 	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"os"
 )
+
+const (
+	// WatchNamespaceEnvVar is the constant for env variable WATCH_NAMESPACE
+	// which is the namespace that the pod is currently running in.
+	WatchNamespaceEnvVar = "WATCH_NAMESPACE"
+)
+
+// GetWatchNamespace returns the namespace the operator should be watching for changes
+func GetWatchNamespace() (string, error) {
+	ns, found := os.LookupEnv(WatchNamespaceEnvVar)
+	if !found {
+		return "", fmt.Errorf("%s must be set", WatchNamespaceEnvVar)
+	}
+	return ns, nil
+}
 
 func SetNamespace(resource interface{}, component *v1alpha1.Component) {
 	if obj, ok := resource.(metav1.Object); ok {
