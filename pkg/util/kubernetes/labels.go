dc52ffba6c4fa934c6be9798db771869cfdb148c
Charles Moulliard <cmoulliard@redhat.com>
2019-01-29 18:45:32 +0100

    Rename function. Return map[string]string. Add todo to specify the backend using a list

--- pkg/util/kubernetes/labels.go
+++ pkg/util/kubernetes/labels.go
@@ -2,12 +2,12 @@ package kubernetes
 
 import "github.com/snowdrop/component-operator/pkg/apis/component/v1alpha1"
 
-func EnrichComponentWithLabels(component *v1alpha1.Component) {
+func PopulateK8sLabels(component *v1alpha1.Component) map[string]string {
 	labels := map[string]string{}
 	labels[v1alpha1.RuntimeLabelKey] = component.Spec.Runtime
 	labels[v1alpha1.RuntimeVersionLabelKey] = component.Spec.Version
-	labels[v1alpha1.ComponentLabelKey] = "backend"
+	labels[v1alpha1.ComponentLabelKey] = "Backend" // TODO -> Select from a list according to type: frontend, backend, service, ...
 	labels[v1alpha1.NameLabelKey] = component.Name
 	labels[v1alpha1.ManagedByLabelKey] = "component-operator"
-	component.Labels = labels
+	return labels
 }
