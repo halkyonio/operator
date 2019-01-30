bfe94135085ec7664c1c23befd98e1f566034069
Charles Moulliard <cmoulliard@redhat.com>
2019-01-30 16:26:16 +0100

    Review code of outerloop

--- pkg/pipeline/outerloop/install.go
+++ pkg/pipeline/outerloop/install.go
@@ -20,20 +20,13 @@ package outerloop
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha1"
-	"golang.org/x/net/context"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	"github.com/snowdrop/component-operator/pkg/pipeline"
 	"github.com/snowdrop/component-operator/pkg/util/kubernetes"
 	util "github.com/snowdrop/component-operator/pkg/util/template"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"strings"
 
 	"k8s.io/apimachinery/pkg/runtime"
-	"text/template"
 )
 
 // NewInstallStep creates a step that handles the creation of the BuilcConfig
@@ -68,7 +61,7 @@ func installOuterLoop(component *v1alpha1.Component, c client.Client, namespace 
 	if isOpenshift {
 		tmpl, ok := util.Templates["outerloop/imagestream"]
 		if ok {
-			err := createResource(tmpl, component, c, &scheme)
+			err := kubernetes.CreateResource(tmpl, component, c, &scheme)
 			if err != nil {
 				return err
 			}
@@ -77,80 +70,12 @@ func installOuterLoop(component *v1alpha1.Component, c client.Client, namespace 
 
 		tmpl, ok = util.Templates["outerloop/buildconfig"]
 		if ok {
-			err := createResource(tmpl, component, c, &scheme)
+			err := kubernetes.CreateResource(tmpl, component, c, &scheme)
 			if err != nil {
 				return err
 			}
 			log.Infof("### Created Buildconfig")
 		}
 	}
-
-	log.Info("## Pipeline 'outerloop' ended ##")
-	log.Info("------------------------------------------------------")
 	return nil
-}
-
-func createResource(tmpl template.Template, component *v1alpha1.Component, c client.Client, scheme *runtime.Scheme) error {
-	res, err := newResourceFromTemplate(tmpl, component, scheme)
-	if err != nil {
-		return err
-	}
-
-	for _, r := range res {
-		if obj, ok := r.(metav1.Object); ok {
-			obj.SetLabels(kubernetes.PopulateK8sLabels(component, "Backend"))
-		}
-		err = c.Create(context.TODO(), r)
-		if err != nil && k8serrors.IsNotFound(err) {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func newResourceFromTemplate(template template.Template, component *v1alpha1.Component, scheme *runtime.Scheme) ([]runtime.Object, error) {
-	var result = []runtime.Object{}
-
-	var b = util.Parse(template, component)
-	r, err := kubernetes.PopulateKubernetesObjectFromYaml(b.String())
-	if err != nil {
-		return nil, err
-	}
-
-	if strings.HasSuffix(r.GetKind(), "List") {
-		l, err := r.ToList()
-		if err != nil {
-			return nil, err
-		}
-		for _, item := range l.Items {
-			obj, err := kubernetes.RuntimeObjectFromUnstructured(&item)
-			if err != nil {
-				return nil, err
-			}
-			ro, ok := obj.(v1.Object)
-			ro.SetNamespace(component.Namespace)
-			if !ok {
-				return nil, err
-			}
-			controllerutil.SetControllerReference(component, ro, scheme)
-			//kubernetes.SetNamespaceAndOwnerReference(obj, component)
-			result = append(result, obj)
-		}
-	} else {
-		obj, err := kubernetes.RuntimeObjectFromUnstructured(r)
-		if err != nil {
-			return nil, err
-		}
-
-		ro, ok := obj.(v1.Object)
-		ro.SetNamespace(component.Namespace)
-		if !ok {
-			return nil, err
-		}
-		controllerutil.SetControllerReference(component, ro, scheme)
-		//kubernetes.SetNamespaceAndOwnerReference(obj, component)
-		result = append(result, obj)
-	}
-	return result, nil
 }
