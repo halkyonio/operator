dbb90938e08e3bb2960a110b0f004358044b0ceb
Charles Moulliard <cmoulliard@redhat.com>
2019-05-03 16:04:04 +0200

    Fix #42. Fetch resources to check if the Buildconfig or ImageStream already exists

--- pkg/pipeline/outerloop/install.go
+++ pkg/pipeline/outerloop/install.go
@@ -18,8 +18,12 @@ limitations under the License.
 package outerloop
 
 import (
+	"context"
 	log "github.com/sirupsen/logrus"
 	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha1"
+	build "github.com/openshift/api/build/v1"
+	image "github.com/openshift/api/image/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -62,21 +66,47 @@ func installOuterLoop(component v1alpha1.Component, config rest.Config, c client
 	if isOpenshift {
 		tmpl, ok := util.Templates["outerloop/imagestream"]
 		if ok {
-			err := kubernetes.CreateResource(tmpl, &component, c, &scheme)
+			// Check if an ImageStream already exists
+			is, err := fetchImageStream(c, &component)
 			if err != nil {
-				return err
+				err = kubernetes.CreateResource(tmpl, &component, c, &scheme)
+				if err != nil {
+					return err
+				}
+				log.Infof("### Created ImageStream used as target image to run the application")
+			} else {
+				log.Infof("### Image stream already exists %s",is.Name)
 			}
-			log.Infof("### Created Imagestream used as target image to run the application")
 		}
 
 		tmpl, ok = util.Templates["outerloop/buildconfig"]
 		if ok {
-			err := kubernetes.CreateResource(tmpl, &component, c, &scheme)
+			// Check if a BuildConfig already exists
+			bc, err := fetchBuildConfig(c, &component)
 			if err != nil {
-				return err
+				err := kubernetes.CreateResource(tmpl, &component, c, &scheme)
+				if err != nil {
+					return err
+				}
+				log.Infof("### Created Buildconfig")
+			} else {
+				log.Infof("### BuildConfig already exists: %s",bc.Name)
 			}
-			log.Infof("### Created Buildconfig")
 		}
 	}
 	return nil
+}
+
+func fetchBuildConfig(c client.Client, component *v1alpha1.Component) (*build.BuildConfig, error) {
+	log.Info("## Checking if the BuilConfig already exists")
+	buildConfig := &build.BuildConfig{}
+	err := c.Get(context.TODO(), types.NamespacedName{Name: component.Name, Namespace: component.Namespace}, buildConfig)
+	return buildConfig, err
+}
+
+func fetchImageStream(c client.Client, component *v1alpha1.Component) (*image.ImageStream, error) {
+	log.Info("## Checking if the ImageStream already exists")
+	is := &image.ImageStream{}
+	err := c.Get(context.TODO(), types.NamespacedName{Name: component.Name, Namespace: component.Namespace}, is)
+	return is, err
 }
