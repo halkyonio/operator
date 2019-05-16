8eb54c8579236d8279c49bac667d31a77243e754
Charles Moulliard <cmoulliard@redhat.com>
2019-05-16 17:55:41 +0200

    Hard code the name of the java runtime image till the function is externalized

--- pkg/controller/component/deployment.go
+++ pkg/controller/component/deployment.go
@@ -10,6 +10,59 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
+/*
+DC of build config
+
+apiVersion: apps.openshift.io/v1
+kind: DeploymentConfig
+metadata:
+  labels:
+    app: {{.Name}}{{ range $key, $value := .ObjectMeta.Labels }}
+    {{ $key }}: {{ $value }}{{ end }}
+  name: {{.Name}}
+spec:
+  replicas: 1
+  selector:
+    app: {{.Name}}
+    deploymentconfig: {{.Name}}
+  strategy:
+    type: Rolling
+  template:
+    metadata:
+      labels:
+        app: {{.Name}}{{ range $key, $value := .ObjectMeta.Labels }}
+        {{ $key }}: {{ $value }}{{ end }}
+        deploymentconfig: {{.Name}}
+      name: {{.Name}}
+    spec:
+      containers:
+      - env:
+        {{ range .Spec.Envs }}
+        - name: {{.Name}}
+          value: {{.Value}}
+        {{end}}
+        image: {{ index .ObjectMeta.Annotations "app.openshift.io/runtime-image" }}
+        name: {{.Name}}
+        ports:
+        - containerPort: {{.Spec.Port}}
+          protocol: TCP
+  triggers:
+  - type: ImageChange
+    imageChangeParams:
+      automatic: true
+      containerNames:
+      - {{.Name}}
+      from:
+        kind: ImageStreamTag
+        name: {{ index .ObjectMeta.Annotations "app.openshift.io/runtime-image" }}:latest
+
+*/
+
+const (
+	SupervisorContainerName = "copy-supervisord"
+	SupervisorImageName = "supervisord"
+)
+
 //buildDeployment returns the Deployment config object
 func (r *ReconcileComponent) buildDeployment(c *v1alpha2.Component) *appsv1.Deployment {
 	ls := r.getAppLabels(c.Name)
@@ -45,7 +98,15 @@ func (r *ReconcileComponent) buildDeployment(c *v1alpha2.Component) *appsv1.Depl
 							"/var/lib/supervisord/bin/supervisord",
 						},
 						Env:             *r.populatePodEnvVar(c),
-						Image:           c.Spec.GetImageReference(),
+						/* TODO: To be changed
+						We can't fetch the runtime image name calculated -> dev-runtime-.... as we will use one of these images
+						according to runtime type to run supervisord + java app OR supervisord + Node
+						https://github.com/snowdrop/component-operator/blob/master/pkg/pipeline/innerloop/images.go#L31-L35
+
+						image["java"] = "quay.io/snowdrop/spring-boot-s2i"
+						image["nodejs"] = "nodeshift/centos7-s2i-nodejs:10.x"
+						*/
+						Image: "quay.io/snowdrop/spring-boot-s2i:latest",
 						ImagePullPolicy: corev1.PullAlways,
 						Name:            c.Name,
 						Ports: []corev1.ContainerPort{{
@@ -64,8 +125,8 @@ func (r *ReconcileComponent) buildDeployment(c *v1alpha2.Component) *appsv1.Depl
 								Value: "run-java:/usr/local/s2i/run;run-node:/usr/libexec/s2i/run;compile-java:/usr/local/s2i/assemble;build:/deployments/buildapp"}},
 						Image:                    util.GetImageReference(SupervisorImageName),
 						ImagePullPolicy:          corev1.PullAlways,
-						Name:                     SupervisorImageName,
-						TerminationMessagePath:   "dev/termination-log",
+						Name:                     SupervisorContainerName,
+						TerminationMessagePath:   "/dev/termination-log",
 						TerminationMessagePolicy: "File",
 						VolumeMounts: []corev1.VolumeMount{
 							{Name: "shared-data", MountPath: "/var/lib/supervisord"},
