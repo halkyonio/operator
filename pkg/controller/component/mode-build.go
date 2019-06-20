29c0fd9a37508fac258cf149949872fa02054ecd
Chris Laprun <metacosm@gmail.com>
2019-06-20 16:48:40 +0200

    refactor: unify handling of Deployment as a dependentResource

--- pkg/controller/component/mode-build.go
+++ pkg/controller/component/mode-build.go
@@ -19,12 +19,10 @@ package component
 
 import (
 	"context"
-	"fmt"
 	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha2"
 	pipeline "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
-	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 func (r *ReconcileComponent) installBuildMode(component *v1alpha2.Component, namespace string) (bool, error) {
@@ -44,7 +42,7 @@ func (r *ReconcileComponent) installBuildMode(component *v1alpha2.Component, nam
 		return false, e
 	}
 
-	if e := r.createDeploymentForBuildMode(component, hasChanges); e != nil {
+	if e := r.createAndCheckForChanges(component, &v1.Deployment{}, hasChanges); e != nil {
 		return false, e
 	}
 
@@ -53,57 +51,6 @@ func (r *ReconcileComponent) installBuildMode(component *v1alpha2.Component, nam
 	}
 
 	return *hasChanges, nil
-}
-
-func (r *ReconcileComponent) createDeploymentForBuildMode(component *v1alpha2.Component, hasChanges *bool) error {
-
-	if _, e := r.fetchDeployment(component.Namespace, component.Name + "-build"); e == nil {
-		r.reqLogger.Info("Deployment object already exist for the runtime container.")
-		return nil
-	}
-
-	// Create a new Deployment resource for a runtime container where the pod is created using a
-	// docker image
-	obj, e := r.createBuildDeployment(component)
-	if e != nil {
-		return fmt.Errorf("Deployment for the runtime container can't be created.")
-	}
-
-	buildDeployment := obj.(*appsv1.Deployment)
-	buildDeployment.Name = component.Name + "-build"
-	controllerutil.SetControllerReference(component, buildDeployment, r.scheme)
-
-	// We will check if a Dev Deployment exists.
-	// If this is the case, then that means that we are switching from dev to build mode
-	// and we will enrich the deployment resource of the runtime container
-	devDeployment, e := r.fetchDeployment(component.Namespace, component.Name)
-	if e == nil {
-		devContainer := &devDeployment.Spec.Template.Spec.Containers[0]
-		buildContainer := &buildDeployment.Spec.Template.Spec.Containers[0]
-		buildContainer.Env = devContainer.Env
-		buildContainer.EnvFrom = devContainer.EnvFrom
-		buildContainer.Env = r.UpdateEnv(buildContainer.Env, component.Annotations["app.openshift.io/java-app-jar"])
-	}
-
-	// Create the Deployment object
-	e = r.client.Create(context.TODO(), buildDeployment)
-	if e != nil {
-		return fmt.Errorf("Failed to create new deployment for the runtime container, %s",e.Error())
-	}
-	*hasChanges = true
-	return nil
-}
-
-func (r *ReconcileComponent) UpdateEnv(envs []corev1.EnvVar, jarName string) []corev1.EnvVar {
-	newEnvs := []corev1.EnvVar{}
-	for _, s := range envs {
-		if s.Name == "JAVA_APP_JAR" {
-			newEnvs = append(newEnvs, corev1.EnvVar{Name: s.Name, Value: jarName})
-		} else {
-			newEnvs = append(newEnvs, s)
-		}
-	}
-	return newEnvs
 }
 
 func (r *ReconcileComponent) updateServiceSelector(component *v1alpha2.Component, hasChanges *bool) error {
@@ -118,7 +65,7 @@ func (r *ReconcileComponent) updateServiceSelector(component *v1alpha2.Component
 
 	if svc, e := r.fetchService(component); e != nil {
 		// Service don't exist. So will create it
-		obj, e := r.buildService(dependentResource{prototype: &corev1.Service{},name:defaultNamer}, component)
+		obj, e := r.buildService(dependentResource{prototype: &corev1.Service{}, name: defaultNamer}, component)
 		if e != nil {
 			svc := obj.(*corev1.Service)
 			svc.Spec.Selector = map[string]string{
