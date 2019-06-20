92b70f3a0f5e5d2c0abee5f9ff649fa90084dd30
Chris Laprun <metacosm@gmail.com>
2019-06-20 17:39:13 +0200

    refactor: remove now unneeded fetches

--- pkg/controller/component/fetches.go
+++ pkg/controller/component/fetches.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha2"
+	tektonv1alpha1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	appsv1 "k8s.io/api/apps/v1"
-	tektonv1alpha1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
 )
 
 // Request object not found, could have been deleted after reconcile request.
@@ -30,18 +29,6 @@ func (r *ReconcileComponent) fetchComponent(request reconcile.Request) (*v1alpha
 	component := &v1alpha2.Component{}
 	err := r.client.Get(context.TODO(), request.NamespacedName, component)
 	return component, err
-}
-
-func (r *ReconcileComponent) fetchService(c *v1alpha2.Component) (*corev1.Service, error) {
-	service := &corev1.Service{}
-	err := r.client.Get(context.TODO(), types.NamespacedName{Name: c.Name, Namespace: c.Namespace}, service)
-	return service, err
-}
-
-func (r *ReconcileComponent) fetchDeployment(namespace, name string) (*appsv1.Deployment, error) {
-	deployment := &appsv1.Deployment{}
-	err := r.client.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: namespace}, deployment)
-	return deployment, err
 }
 
 func (r *ReconcileComponent) fetchTaskRun(c *v1alpha2.Component) (*tektonv1alpha1.TaskRun, error) {
