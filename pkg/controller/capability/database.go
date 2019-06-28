ab7ad4747cf4b45489b638a8cbf43856358c648b
Charles Moulliard <cmoulliard@redhat.com>
2019-06-28 07:38:34 +0200

    WIP. Test database kind and only support Postgres

--- pkg/controller/capability/database.go
+++ pkg/controller/capability/database.go
@@ -1,6 +1,8 @@
 package capability
 
 import (
+	"errors"
+	"fmt"
 	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha2"
 )
 
@@ -21,13 +23,17 @@ func (r *ReconcileCapability) installDB(c *v1alpha2.Capability) (bool, error) {
 		}
 	}
 
-	// Check if the KubeDB - Postgres exists
-	if _, e := r.fetchKubeDBPostgres(c); e != nil {
-		if e = r.create(c, PG_DATABASE); e != nil {
-			return false, e
-		} else {
-			*hasChanges = true
+	if string(c.Spec.Kind) == string(v1alpha2.PostgresKind) {
+		// Check if the KubeDB - Postgres exists
+		if _, e := r.fetchKubeDBPostgres(c); e != nil {
+			if e = r.create(c, PG_DATABASE); e != nil {
+				return false, e
+			} else {
+				*hasChanges = true
+			}
 		}
+	} else {
+		return false, fmt.Errorf("Database kind not supported %s", c.Spec.Kind)
 	}
 
 	return *hasChanges, nil
