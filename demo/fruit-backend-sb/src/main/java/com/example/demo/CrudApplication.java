dc163172e7b280a4d5a77fa0331b1b6f0ec4d0ca
Charles Moulliard <cmoulliard@redhat.com>
2019-07-09 18:34:43 +0200

    Use @Capability annotation to test it. Rename modules to use Fruit within the name

--- demo/fruit-backend-sb/src/main/java/com/example/demo/CrudApplication.java
+++ demo/fruit-backend-sb/src/main/java/com/example/demo/CrudApplication.java
@@ -16,6 +16,7 @@
 
 package com.example.demo;
 
+import io.dekorate.component.annotation.Capability;
 import io.dekorate.component.annotation.ComponentApplication;
 import io.dekorate.component.annotation.Link;
 import io.dekorate.component.model.Kind;
@@ -35,11 +36,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
     componentName = "fruit-backend-sb",
     kind = Kind.Secret,
     ref = "postgresql-db")
+@Capability(
+    name = "postgres-db",
+    category = "database",
+    kind = "postgres",
+    version = "10",
+    parameters = {
+       @Parameter(name = "DB_USER", value = "admin"),
+       @Parameter(name = "DB_PASSWORD", value = "admin"),
+       @Parameter(name = "DB_NAME", value = "sample-db"),
+    }
+)
 @SpringBootApplication
-public class CrudApplication {
-
+public class CrudApplication
     public static void main(String[] args) {
         SpringApplication.run(CrudApplication.class, args);
     }
-
 }
