fca93b807a6e5baa7d236ecddcfd86a00415d215
Chris Laprun <metacosm@gmail.com>
2019-07-17 13:07:27 +0200

    feat: add Index method to check if a string is contained in an array

--- pkg/util/commons.go
+++ pkg/util/commons.go
@@ -21,3 +21,12 @@ func GetObjectName(object runtime.Object) string {
 	}
 	return t.Name()
 }
+
+func Index(vs []string, t string) int {
+	for i, v := range vs {
+		if v == t {
+			return i
+		}
+	}
+	return -1
+}
