2721a9d088eef595ebea9bbc5afae3d3b2ddaa4a
Chris Laprun <metacosm@gmail.com>
2019-07-23 12:57:51 +0200

    fix: remove duplicated section

--- demo/scripts/end-to-end_fruit_demo.sh
+++ demo/scripts/end-to-end_fruit_demo.sh
@@ -76,16 +76,6 @@ kubectl create ns "${NS}"
 printTitle "Add privileged SCC to the serviceaccount postgres-db and buildbot. Required for the operators Tekton and KubeDB"
 if [ "$isOpenShift" == "true" ]; then
   echo "We run on Openshift. So we will apply the SCC rule"
-  oc adm policy add-scc-to-user privileged system:serviceaccount:"${NS}":postgres-db
-  oc adm policy add-scc-to-user privileged system:serviceaccount:"${NS}":build-bot
-  oc adm policy add-role-to-user edit system:serviceaccount:"${NS}":build-bot
-else
-  echo "We DON'T run on OpenShift. So need to change SCC"
-fi
-
-printTitle "Add privileged SCC to the serviceaccount postgres-db and buildbot. Required for the operators Tekton and KubeDB"
-if [ "$isOpenShift" == "true" ]; then
-  echo "We run on Openshift. So we will apply the SCC rule"
   oc adm policy add-scc-to-user privileged system:serviceaccount:${NS}:postgres-db
   oc adm policy add-scc-to-user privileged system:serviceaccount:${NS}:build-bot
   oc adm policy add-role-to-user edit system:serviceaccount:${NS}:build-bot
