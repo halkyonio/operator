66a4e6f21cf287b1e58376c652e15305d169b72b
Charles Moulliard <cmoulliard@redhat.com>
2019-08-07 14:27:10 +0200

    Add missing resources to be created

---
+++ pkg/controller/k8srolebinding.go
@@ -0,0 +1,60 @@
+package controller
+
+import (
+	authorizv1 "k8s.io/api/rbac/v1"
+	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type k8srolebinding struct {
+	*DependentResourceHelper
+}
+
+func (res k8srolebinding) Update(toUpdate runtime.Object) (bool, error) {
+	return false, nil
+}
+
+func (res k8srolebinding) NewInstanceWith(owner v1alpha2.Resource) DependentResource {
+	return newOwnedk8sRoleBinding(owner)
+}
+
+func Newk8sRoleBinding() k8srolebinding {
+	return newOwnedk8sRoleBinding(nil)
+}
+
+func newOwnedk8sRoleBinding(owner v1alpha2.Resource) k8srolebinding {
+	dependent := NewDependentResource(&authorizv1.RoleBinding{}, owner)
+	rolebinding := k8srolebinding{
+		DependentResourceHelper: dependent,
+	}
+	dependent.SetDelegate(rolebinding)
+	return rolebinding
+}
+
+func (res k8srolebinding) Name() string {
+	return "use-scc-privileged"
+}
+
+func (res k8srolebinding) Build() (runtime.Object, error) {
+	c := res.Owner()
+	namespace := c.GetNamespace()
+	ser := &authorizv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      res.Name(),
+			Namespace: namespace,
+		},
+		RoleRef: authorizv1.RoleRef{
+			Kind:       "Role",
+			Name:       RoleName(c),
+		},
+		Subjects: []authorizv1.Subject{
+			{Kind: "ServiceAccount", Name: ServiceAccountName(c), Namespace: namespace},
+		},
+	}
+	return ser, nil
+}
+
+func (res k8srolebinding) ShouldWatch() bool {
+	return false
+}
