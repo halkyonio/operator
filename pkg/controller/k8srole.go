f67f1d2119da2108310c8517a9c48c6250b9f7e4
Charles Moulliard <cmoulliard@redhat.com>
2019-08-07 14:27:10 +0200

    Add missing resources to be created

---
+++ pkg/controller/k8srole.go
@@ -0,0 +1,58 @@
+package controller
+
+import (
+	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha2"
+	authorizv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type k8srole struct {
+	*DependentResourceHelper
+}
+
+func (res k8srole) Update(toUpdate runtime.Object) (bool, error) {
+	return false, nil
+}
+
+func (res k8srole) NewInstanceWith(owner v1alpha2.Resource) DependentResource {
+	return newOwnedk8srole(owner)
+}
+
+func Newk8sRole() k8srole {
+	return newOwnedk8srole(nil)
+}
+
+func newOwnedk8srole(owner v1alpha2.Resource) k8srole {
+	dependent := NewDependentResource(&authorizv1.Role{}, owner)
+	role := k8srole{DependentResourceHelper: dependent}
+	dependent.SetDelegate(role)
+	return role
+}
+
+func (res k8srole) Name() string {
+	return RoleName(res.Owner())
+}
+
+func (res k8srole) Build() (runtime.Object, error) {
+	c := res.Owner()
+	ser := &authorizv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      res.Name(),
+			Namespace: c.GetNamespace(),
+		},
+		Rules: []authorizv1.PolicyRule{
+			{
+				APIGroups:     []string{"security.openshift.io"},
+				Resources:     []string{"securitycontextconstraints"},
+				ResourceNames: []string{"privileged"},
+				Verbs:         []string{"use"},
+			},
+		},
+	}
+	return ser, nil
+}
+
+func (res k8srole) ShouldWatch() bool {
+	return false
+}
