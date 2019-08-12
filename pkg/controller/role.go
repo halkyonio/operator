ad0edf0047e755f5be2d16bcb9e71fc5426f99a5
Chris Laprun <metacosm@gmail.com>
2019-08-12 21:33:43 +0200

    refactor: use generic role/role binding, fix improper role ref

---
+++ pkg/controller/role.go
@@ -0,0 +1,79 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha2"
+	authorizv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type role struct {
+	*DependentResourceHelper
+}
+
+func (res role) Update(toUpdate runtime.Object) (bool, error) {
+	return false, nil
+}
+
+func (res role) NewInstanceWith(owner v1alpha2.Resource) DependentResource {
+	return newOwnedRole(owner)
+}
+
+func NewRole() role {
+	return newOwnedRole(nil)
+}
+
+func newOwnedRole(owner v1alpha2.Resource) role {
+	dependent := NewDependentResource(&authorizv1.Role{}, owner)
+	role := role{DependentResourceHelper: dependent}
+	dependent.SetDelegate(role)
+	return role
+}
+
+func RoleName(owner v1alpha2.Resource) string {
+	switch owner.(type) {
+	case *v1alpha2.Component:
+		return "image-scc-privileged-role"
+	case *v1alpha2.Capability:
+		return "scc-privileged-role"
+	default:
+		panic(fmt.Sprintf("unknown type '%s' for role owner", GetObjectName(owner)))
+	}
+}
+
+func (res role) Name() string {
+	return RoleName(res.Owner())
+}
+
+func (res role) Build() (runtime.Object, error) {
+	c := res.Owner()
+	ser := &authorizv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      res.Name(),
+			Namespace: c.GetNamespace(),
+		},
+		Rules: []authorizv1.PolicyRule{
+			{
+				APIGroups:     []string{"security.openshift.io"},
+				Resources:     []string{"securitycontextconstraints"},
+				ResourceNames: []string{"privileged"},
+				Verbs:         []string{"use"},
+			},
+		},
+	}
+
+	if _, ok := c.(*v1alpha2.Component); ok {
+		ser.Rules = append(ser.Rules, authorizv1.PolicyRule{
+			APIGroups: []string{"image.openshift.io"},
+			Resources: []string{"imagestreams", "imagestreams/layers"},
+			Verbs:     []string{"*"},
+		})
+	}
+
+	return ser, nil
+}
+
+func (res role) ShouldWatch() bool {
+	return false
+}
---
+++ pkg/controller/rolebinding.go
@@ -0,0 +1,100 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/snowdrop/component-operator/pkg/apis/component/v1alpha2"
+	authorizv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+type roleBinding struct {
+	*DependentResourceHelper
+}
+
+func (res roleBinding) Update(toUpdate runtime.Object) (bool, error) {
+	// add appropriate subject for owner
+	rb := toUpdate.(*authorizv1.RoleBinding)
+	owner := res.Owner()
+
+	// check if the binding contains the current owner as subject
+	namespace := owner.GetNamespace()
+	name := ServiceAccountName(owner)
+	found := false
+	for _, subject := range rb.Subjects {
+		if subject.Name == name && subject.Namespace == namespace {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		rb.Subjects = append(rb.Subjects, authorizv1.Subject{
+			Kind:      "ServiceAccount",
+			Namespace: namespace,
+			Name:      name,
+		})
+	}
+
+	return !found, nil
+}
+
+func (res roleBinding) NewInstanceWith(owner v1alpha2.Resource) DependentResource {
+	return newOwnedRoleBinding(owner)
+}
+
+func NewRoleBinding() roleBinding {
+	return newOwnedRoleBinding(nil)
+}
+
+func newOwnedRoleBinding(owner v1alpha2.Resource) roleBinding {
+	dependent := NewDependentResource(&authorizv1.RoleBinding{}, owner)
+	rolebinding := roleBinding{
+		DependentResourceHelper: dependent,
+	}
+	dependent.SetDelegate(rolebinding)
+	return rolebinding
+}
+
+func RoleBindingName(owner v1alpha2.Resource) string {
+	switch owner.(type) {
+	case *v1alpha2.Component:
+		return "use-image-scc-privileged"
+	case *v1alpha2.Capability:
+		return "use-scc-privileged"
+	default:
+		panic(fmt.Sprintf("unknown type '%s' for role owner", GetObjectName(owner)))
+	}
+}
+
+func (res roleBinding) Name() string {
+	return RoleBindingName(res.Owner())
+}
+
+func (res roleBinding) Build() (runtime.Object, error) {
+	c := res.Owner()
+	namespace := c.GetNamespace()
+	ser := &authorizv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      res.Name(),
+			Namespace: namespace,
+		},
+		RoleRef: authorizv1.RoleRef{
+			Kind: "Role",
+			Name: RoleName(c),
+		},
+		Subjects: []authorizv1.Subject{
+			{Kind: "ServiceAccount", Name: ServiceAccountName(c), Namespace: namespace},
+		},
+	}
+
+	if _, ok := c.(*v1alpha2.Capability); ok {
+		ser.Subjects = append(ser.Subjects, authorizv1.Subject{Kind: "ServiceAccount", Name: PostgresName(c), Namespace: namespace})
+	}
+
+	return ser, nil
+}
+
+func (res roleBinding) ShouldWatch() bool {
+	return false
+}
