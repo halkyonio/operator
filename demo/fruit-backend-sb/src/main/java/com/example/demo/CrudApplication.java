5efd8303e249c000da688c506fb16a41f72a9615
Chris Laprun <metacosm@gmail.com>
2019-08-20 18:19:53 +0200

    fix: update demo application to use proper dekorate version

--- demo/fruit-backend-sb/src/main/java/com/example/demo/CrudApplication.java
+++ demo/fruit-backend-sb/src/main/java/com/example/demo/CrudApplication.java
@@ -20,8 +20,7 @@ import io.dekorate.halkyon.annotation.HalkyonCapability;
 import io.dekorate.halkyon.annotation.HalkyonComponent;
 import io.dekorate.halkyon.annotation.HalkyonLink;
 import io.dekorate.halkyon.annotation.Parameter;
-import io.dekorate.halkyon.model.Kind;
-import io.dekorate.kubernetes.annotation.Env;
+import io.dekorate.halkyon.model.Type;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -32,7 +31,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @HalkyonLink(
     name = "link-to-database",
     componentName = "fruit-backend-sb",
-    kind = Kind.Secret,
+    type = Type.Secret,
     ref = "postgres-db-config")
 @HalkyonCapability(
     name = "postgres-db",
