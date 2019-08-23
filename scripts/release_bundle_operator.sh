7227f8335af24884fdb3d53101f3c21e51c92200
Charles Moulliard <cmoulliard@redhat.com>
2019-08-23 11:26:54 +0200

    Rename the org of the application to use halkyon. Add verbose option

--- scripts/release_bundle_operator.sh
+++ scripts/release_bundle_operator.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# Command: ./release_bundle_operator.sh QUAY_USER QUAY_PASSWORD
+# Command: ./scripts/release_bundle_operator.sh QUAY_USER QUAY_PASSWORD
 
 export QUAY_USER=$1
 export QUAY_PWD=$2
 export AUTH_TOKEN=$(curl -X POST -H "Content-Type: application/json" -d '{"user":{"username":"'"$QUAY_USER"'","password":"'"$QUAY_PWD"'"}}' https://quay.io/cnr/api/v1/users/login | jq -r '.token')
 export QUAY_ORG="halkyonio"
-export REPOSITORY="operator"
+export REPOSITORY="halkyon"
 export RELEASE="0.0.1"
 export BUNDLE_DIR="deploy/olm-catalog/bundle"
 
-operator-courier verify $BUNDLE_DIR
-operator-courier push $BUNDLE_DIR $QUAY_ORG $REPOSITORY $RELEASE "$AUTH_TOKEN"
+operator-courier --verbose verify $BUNDLE_DIR
+operator-courier --verbose push $BUNDLE_DIR $QUAY_ORG $REPOSITORY $RELEASE "$AUTH_TOKEN"
