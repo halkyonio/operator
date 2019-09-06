8575c6c25da8e4d07cd954b1da82456061422be1
Chris Laprun <metacosm@gmail.com>
2019-09-06 15:09:02 +0200

    Revert "feat: Backup content uploaded within folder /deployments. #141"
    This reverts commit a67b978a

--- pkg/controller/component/dev_deployment.go
+++ pkg/controller/component/dev_deployment.go
@@ -40,7 +40,6 @@ func (res deployment) installDev() (runtime.Object, error) {
 		Protocol:      "TCP",
 	}}
 	runtimeContainer.VolumeMounts = append(runtimeContainer.VolumeMounts, corev1.VolumeMount{Name: c.Spec.Storage.Name, MountPath: "/tmp/artifacts"})
-	runtimeContainer.VolumeMounts = append(runtimeContainer.VolumeMounts, corev1.VolumeMount{Name: c.Spec.Storage.Name, MountPath: "/deployments"})
 
 	// create the supervisor init container
 	supervisorContainer, err := r.getBaseContainerFor(r.supervisor)
