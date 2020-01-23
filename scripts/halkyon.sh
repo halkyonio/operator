c67e80d68cbb7a7b9807a952588f18a0eeef015e
Chris Laprun <metacosm@gmail.com>
2020-01-23 21:38:58 +0100

    fix: apply recursively

--- scripts/halkyon.sh
+++ scripts/halkyon.sh
@@ -11,7 +11,7 @@ if [ "$delete" != delete ]; then
     echo "Creating namespace"
     kubectl create ns "${namespace}"
   fi
-  kubectl apply -f deploy/cluster-wide
+  kubectl apply -R -f deploy/cluster-wide
   kubectl apply -n "${namespace}" -f deploy/namespaced
 else
   echo "Deleting Halkyon from namespace ${namespace}"
